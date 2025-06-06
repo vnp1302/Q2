cc5f4e39300b3bed367cdc88b18bbacf3c3fa54b
vnp1302 <vnp1302@gmail.com>
2025-06-06 15:09:14 -0700

    Create CustomHead.tsx

---
+++ components/CustomHead.tsx
@@ -0,0 +1,95 @@
+import Head from 'next/head'
+
+import { useRouter } from 'next/router'
+
+
+
+interface CustomHeadProps {
+
+  title?: string
+
+  description?: string
+
+  keywords?: string[]
+
+  ogImage?: string
+
+}
+
+
+
+export const CustomHead = ({
+
+  title = 'Q1 Token - Multi-Token Ecosystem',
+
+  description = 'Advanced ERC1155 token system on Ethereum and Polygon',
+
+  keywords = ['ERC1155', 'Blockchain', 'Ethereum', 'Polygon', 'Token'],
+
+  ogImage = '/images/og-default.jpg'
+
+}: CustomHeadProps) => {
+
+  const { asPath } = useRouter()
+
+  const canonicalUrl = `https://q1token.com${asPath}`
+
+
+
+  return (
+
+    <Head>
+
+      <title>{title}</title>
+
+      <meta name="description" content={description} />
+
+      <meta name="keywords" content={keywords.join(', ')} />
+
+      <link rel="canonical" href={canonicalUrl} />
+
+      
+
+      {/* Open Graph */}
+
+      <meta property="og:title" content={title} />
+
+      <meta property="og:description" content={description} />
+
+      <meta property="og:image" content={ogImage} />
+
+      <meta property="og:url" content={canonicalUrl} />
+
+      <meta property="og:type" content="website" />
+
+      
+
+      {/* Twitter */}
+
+      <meta name="twitter:card" content="summary_large_image" />
+
+      <meta name="twitter:site" content="@q1token" />
+
+      
+
+      {/* Favicon */}
+
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+      
+
+      {/* PWA */}
+
+      <link rel="manifest" href="/manifest.json" />
+
+      <meta name="theme-color" content="#4F46E5" />
+
+    </Head>
+
+  )
+
+}
