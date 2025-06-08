62a496a4f1841f99c1cde15b4257beea863cd5c1
vnp1302 <vnp1302@gmail.com>
2025-06-07 18:46:56 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/old-page",
-        destination: "/new-page",
-        permanent: true,
-      },
-    ]
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://api.example.com/:path*",
-      },
-    ]
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
