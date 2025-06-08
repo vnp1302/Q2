37af6532fcf1738aae71eef06d8fcfe596eaabdc
vnp1302 <vnp1302@gmail.com>
2025-06-07 17:38:57 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+
+export default function Page() {
+  return (
+    <Layout>
+      <div className="container mx-auto py-8">
+        <h1 className="text-2xl font-bold mb-4">Welcome to Q2 Project</h1>
+        <p className="mb-4">This is the home page of the Q2 Web3 project.</p>
+        {/* Add more content here */}
+      </div>
+    </Layout>
+  )
+}
